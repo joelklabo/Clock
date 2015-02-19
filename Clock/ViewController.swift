04884e663cc6f161a178c23bee26a6d51b72672a
Joel Klabo <jklabo@yammer-inc.com>
2015-02-19 14:09:04 -0800

    Initial Commit

---
+++ Clock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by Joel Klabo on 2/19/15.
+//  Copyright (c) 2015 Joel Klabo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
