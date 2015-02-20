980edea9ee1d7510fc8961d9e9292bee00bcd358
Joel Klabo <joelklabo@gmail.com>
2015-02-20 09:18:41 -0800

    Start, stop and pause animation working

--- Clock/ViewController.swift
+++ Clock/ViewController.swift
@@ -13,9 +13,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var clockView: ClockView!
     
+    let zRotationKeyPath = "transform.rotation.z"
+    var stopAnimation = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.rotate()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,35 +33,58 @@ class ViewController: UIViewController {
         
         switch control.selectedSegmentIndex {
         case 0:
-            self.rotate()
+            self.start()
         case 1:
             self.pause()
         case 2:
-            self.reset()
+            self.clear()
         default:
             println("?")
         }
     }
     
     func rotate () {
- 
-        let duration: NSTimeInterval = 0.5
-        let angle: CGFloat = CGFloat(M_PI)
-        var rotateTransform: CGAffineTransform = CGAffineTransformRotate(self.clockView.transform, angle)
         
-        UIView.animateWithDuration(duration, delay: 0, options: UIViewAnimationOptions.CurveLinear, animations: { () -> Void in
-            self.clockView.transform = CGAffineTransformRotate(self.clockView.transform, angle)
-        }) { (Bool) -> Void in
-            self.rotate()
-        }
+        let angleToAdd = CGFloat(M_PI_4) / 2
+        let currentAngle = self.clockView.layer.valueForKeyPath(zRotationKeyPath) as CGFloat
+        let newAngle = currentAngle + angleToAdd
+
+        self.clockView.layer.setValue(newAngle, forKeyPath: zRotationKeyPath)
+        
+        var rotateAnimation = CABasicAnimation(keyPath: zRotationKeyPath)
+        
+        rotateAnimation.duration = 0.0625
+        rotateAnimation.additive = true
+        rotateAnimation.removedOnCompletion = false
+        rotateAnimation.fillMode = kCAFillModeForwards
+        rotateAnimation.toValue = 0.0
+        rotateAnimation.byValue = angleToAdd
+        rotateAnimation.delegate = self
+        
+        self.clockView.layer.addAnimation(rotateAnimation, forKey: "rotation")
     }
 
-    func pause () {
-        println("pause")
+    func start () {
+        stopAnimation = false
+        self.rotate()
     }
     
-    func reset () {
-        println("Clear")
+    func pause () {
+        stopAnimation = true
+    }
+    
+    func clear () {
+        stopAnimation = true
+        self.clockView.layer.setValue(CGFloat(0), forKeyPath: zRotationKeyPath)
+    }
+    
+    // CABasicAnimationDelegate methods
+    
+    
+    override func animationDidStop(anim: CAAnimation!, finished flag: Bool) {
+        if (anim == self.clockView.layer.animationForKey("rotation") && !stopAnimation) {
+            self.rotate()
+        }
     }
 }
 
