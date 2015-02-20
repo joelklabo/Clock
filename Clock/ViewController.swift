56e0aae334ee84865f54a0639a1c40621e9ee93d
Joel Klabo <jklabo@yammer-inc.com>
2015-02-19 19:33:56 -0800

    Animation started, but can't stop yet...

--- Clock/ViewController.swift
+++ Clock/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
 
@@ -23,5 +24,40 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func valueChanged(sender: AnyObject) {
+        let control = sender as UISegmentedControl
+        
+        switch control.selectedSegmentIndex {
+        case 0:
+            self.rotate()
+        case 1:
+            self.pause()
+        case 2:
+            self.reset()
+        default:
+            println("?")
+        }
+    }
+    
+    func rotate () {
+ 
+        let duration: NSTimeInterval = 0.5
+        let angle: CGFloat = CGFloat(M_PI)
+        var rotateTransform: CGAffineTransform = CGAffineTransformRotate(self.clockView.transform, angle)
+        
+        UIView.animateWithDuration(duration, delay: 0, options: UIViewAnimationOptions.CurveLinear, animations: { () -> Void in
+            self.clockView.transform = CGAffineTransformRotate(self.clockView.transform, angle)
+        }) { (Bool) -> Void in
+            self.rotate()
+        }
+    }
+
+    func pause () {
+        println("pause")
+    }
+    
+    func reset () {
+        println("Clear")
+    }
 }
 
